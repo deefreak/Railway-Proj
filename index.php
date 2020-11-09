3d6e4215d97f1b54314183c66ac9bff2dd8fa06f
TanX-357 <59611100+TanX-357@users.noreply.github.com>
2020-11-09 21:41:17 +0530

    Frontend of the main page and contact us page of application.

--- index.php
+++ index.php
@@ -1,40 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<style>
-a:link {
-    color: green;
-}
-a:visited {
-    color: hotpink;
-}
-a:hover {
-    color: red;
-}
-a:active {
-    color: blue;
-}
+<!--
+Design by Bryant Smith
+http://www.bryantsmith.com
+http://www.aszx.net
+email: template [-at-] bryantsmith [-dot-] com
+Released under Creative Commons Attribution 2.5 Generic.  In other words, do with it what you please; but please leave the link if you'd be so kind :)
 
-</style>
+Name       : An Ocean of Sky
+Description: One column, with top naviation.  All divs, validations: XHTML Strict 1.0 & CSS
+Version    : 1.0
+Released   : 20081009
+-->
 
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>RAILWAY RESERVATION SYSTEM</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="mainpage.css" />
+<title></title>
 </head>
-<body style=" background-image: ;
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;" >
-<div align="CENTER" >
-<h1 style="color: red;">RAILWAY RESERVATION SYSTEM</h1>
-<div>
-<h2 style="font-style: italic;"><br><a href="http://localhost/proj1/login.php">User Login </a><br>
-<br><a href="http://localhost/proj1/register.php">Register</a><br>
-<br><a href="http://localhost/proj1/login.php">Admin Login</a><br>
-</h2>
 
-</div>
-</div>
-</body>        
+<body>
+   <div id="page">
+       <div class="topNaviagationLink" ><a href="index.php">Home</a></div>
+       <div class="topNaviagationLink"><a href="http://localhost/proj1/login.php">About</a></div>
+       <div class="topNaviagationLink"><a href="http://localhost/proj1/login.php">User Login</a></div>
+       <div class="topNaviagationLink"><a href="http://localhost/proj1/register.php">Register</a></div>
+       <div class="topNaviagationLink"><a href="http://localhost/proj1/login.php">Admin Login</a></div>
+       <div class="topNaviagationLink"><a href="http://localhost:9090/proj1/contact.php">Contact Us!</a></div>
+     <!-- <div class="topNaviagationLink"><a href="index.html">Contact</a></div> -->
+ </div>
+   <div id="mainPicture">
+     <div class="picture">
+         <div id="headerTitle">RAILWAY RESERVATION PORTAL</div>
+           <div id="headerSubtext"> "Live with no excuses and travel with no regrets"</div>
+       </div>
+   <!-- </div>
+       <div class="contentBox">
+     <div class="innerBox">
+         <div class="contentTitle">The Title of an Article</div>
+         <div class="contentText"><p>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer.   This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :) </p><br />
+       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</p><br />
+         <p>Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.</p></div>
 
+
+       <div class="contentTitle">So Many Titles, So Little Time.</div>
+           <div class="contentText"><p>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer.   This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :) </p><br />
+       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</p><br />
+         <p>Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.</p></div>
+
+
+       <div class="contentTitle">Yet Another  One!</div>
+         <div class="contentText"><p>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer.   This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :) </p><br />
+       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</p><br />
+         <p>Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.</p></div>
+       </div>
+
+   </div>
+       <div id="footer"><a href="http://www.aszx.net">web development</a> by <a href="http://www.bryantsmith.com">bryant smith</a></div> -->
+</body>
 </html>
